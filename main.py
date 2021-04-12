508106ab1e7c208d1e1c84ec19b7746296cfeae1
Ekky Sukmawati <>
2021-04-12 10:10:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#c = a # menampung nilai a ke dalam c 
+#a = b # menampung nilai b ke dalam a
+#b = c # menampung nilai c ke dalam b sehingga nilai asli a yang awal mulanya ditampung c sekarang di tampung b
+
+#Cek Pemahaman
+d = b  # nilai b ditampung ke dalam d
+b = a  # nilai a ditampung ke dalam b
+a = d  # nilai d ditampung ke dalam a sehingga nilai asli b yang awal mulanya ditampung d sekrang ditampung a
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
